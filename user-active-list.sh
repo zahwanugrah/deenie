0491a962ff09d20f5d1a0da51cf5baf71f394b1c
elangoverdosis <deeniedoank@gmail.com>
2017-05-11 01:59:24 +0700

    Add files via upload

---
+++ user-active-list.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+MYIP=$(wget -qO- ipv4.icanhazip.com)
+
+
+
+disable-user-expire
+clear
+
+echo "---------------------------- DAFTAR AKUN SSH ACTIVE ----------------------------"
+
+
+
+	echo "             DEVELOPED BY YUSUF ARDIANSYAH AND ELANG OVERDOSIS           "
+echo ""
+
+cat /root/activeuser.txt
+echo ""
