af2cc68441af52cc03c5d369630933e334554d67
elangoverdosis <deeniedoank@gmail.com>
2017-05-12 22:43:44 +0000

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -331,7 +331,7 @@ wget -O /usr/bin/user-login $source/debian7/user-login.sh
 wget -O /usr/bin/user-pass $source/debian7/user-pass.sh
 wget -O /usr/bin/user-renew $source/debian7/user-renew.sh
 wget -O /usr/bin/clearcache.sh $source/debian7/clearcache.sh
-wget /usr/bin/bannermenu $source/debian7/bannermenu
+wget -O /usr/bin/bannermenu $source/debian7/bannermenu
 
 chmod +x /usr/bin/benchmark
 chmod +x /usr/bin/speedtest
