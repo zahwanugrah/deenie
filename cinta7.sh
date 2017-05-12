932336d17e36edc7abb1c5238103761b21a27fa9
elangoverdosis <deeniedoank@gmail.com>
2017-05-12 21:14:42 +0000

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -355,6 +355,9 @@ chmod +x /usr/bin/user-renew
 echo "*/30 * * * * root service dropbear restart" > /etc/cron.d/dropbear
 echo "00 23 * * * root /usr/bin/disable-user-expire" > /etc/cron.d/disable-user-expire
 echo "0 */12 * * * root /sbin/reboot" > /etc/cron.d/reboot
+echo "00 01 * * * root echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a" > /etc/cron.d/clearcacheram3swap
+echo "*/20 * * * * root ./clearcache.sh" > /etc/cron.d/clearcache1
+
 #echo "@reboot root /usr/bin/user-limit" > /etc/cron.d/user-limit
 #echo "@reboot root /usr/bin/autokill" > /etc/cron.d/autokill
 #sed -i '$ i\screen -AmdS check /root/autokill' /etc/rc.local
