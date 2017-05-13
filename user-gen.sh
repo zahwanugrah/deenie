4051411c5b8770a7f6c2570ab576a4b2bc8e47a5
elangoverdosis <deeniedoank@gmail.com>
2017-05-13 03:34:56 +0000

    Update user-gen.sh

--- user-gen.sh
+++ user-gen.sh
@@ -43,4 +43,28 @@ done
 
 echo "Valid s/d: $(date -d "$AKTIF days" +"%d-%m-%Y")"
 echo "-----------------------------------"
-
+clear
+echo -e ""| lolcat
+echo -e "|       Informasi Akun Baru SSH      |" | boxes -d dog | lolcat
+echo -e "============ -SERVER PREMIUM- =============" | lolcat
+echo -e "     Host: $MYIP" | lolcat
+echo -e "     Username: $username" | lolcat
+echo -e "     Password: $password                   " | lolcat
+echo -e "     Port default dropbear: 443,80         " | lolcat
+echo -e "     Port default openSSH : 22,143         " | lolcat
+echo -e "     Port default squid   : 8080           " | lolcat
+echo -e "     Port default openVPN : 55             " | lolcat
+echo -e "     Auto kill user maximal login 2        " | lolcat
+echo -e "-------------------------------------------" | lolcat
+echo -e "     Aktif Sampai: $(date -d "$AKTIF days" +"%d-%m-%Y")" | lolcat
+echo -e "===========================================" | lolcat
+echo -e "   DI LARANG:                              "| lolcat
+echo -e "   HACKING-DDOS-PHISING-SPAM-TORENT        " | lolcat
+echo -e "   CARDING-CRIMINAL CYBER.                 "| lolcat
+echo -e "===========================================" | lolcat
+echo -e "   Script by Yusuf Ardiansyah              "| lolcat
+echo -e "   Config OVPN:                            "| lolcat
+echo -e "   http://$MYIP:81/client.ovpn        " | lolcat
+echo -e "-------------------------------------------" | lolcat
+echo -e ""
+echo -e ""
