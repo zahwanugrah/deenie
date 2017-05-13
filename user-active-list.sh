d1b19fd3c4a3f5fd35634f8f9f64209c46942b96
elangoverdosis <deeniedoank@gmail.com>
2017-05-13 04:24:22 +0000

    Update user-active-list.sh

--- user-active-list.sh
+++ user-active-list.sh
@@ -12,11 +12,11 @@ MYIP=$(wget -qO- ipv4.icanhazip.com)
 disable-user-expire
 clear
 
-echo "---------------------------- DAFTAR AKUN SSH ACTIVE ----------------------------"
+#echo "---------------------------- DAFTAR AKUN SSH ACTIVE ----------------------------"
 
 
 
-	echo "             DEVELOPED BY YUSUF ARDIANSYAH AND ELANG OVERDOSIS           "
+echo "DEVELOPED BY YUSUF ARDIANSYAH AND ELANG OVERDOSIS"
 echo ""
 
 cat /root/activeuser.txt
