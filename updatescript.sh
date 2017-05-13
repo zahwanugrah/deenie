542f839606894d0d8fd0873645982057903ad263
elangoverdosis <deeniedoank@gmail.com>
2017-05-13 00:40:43 +0000

    Update updatescript.sh

--- updatescript.sh
+++ updatescript.sh
@@ -1,14 +1,14 @@
-		#!/bin/bash
-echo "Maaf.. Saat ini belum tersedia update script bos......!!!" | boxes -d boy
+#!/bin/bash
+#echo "Maaf.. Saat ini belum tersedia update script bos......!!!" | boxes -d boy
 #echo "Update webmin tersedia bos, tp versi webmin 1.820_all.deb..gk apa2 ya boss.." | lolcat
 sleep 4
 cd
-#wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.820_all.deb"
-#dpkg --install webmin_1.820_all.deb
-#apt-get -y -f install
-#rm /root/webmin_1.820_all.deb
-#sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
-#service webmin restart
+wget "http://prdownloads.sourceforge.net/webadmin/webmin_1.820_all.deb"
+dpkg --install webmin_1.820_all.deb
+apt-get -y -f install
+rm /root/webmin_1.820_all.deb
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
 
 clear
 echo "selesai bosss...." | lolcat
