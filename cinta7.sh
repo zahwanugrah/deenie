4c30e41d981e86181c0040c93abe413cfefc8293
elangoverdosis <deeniedoank@gmail.com>
2017-05-14 10:13:34 +0000

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -369,7 +369,67 @@ echo "*/20 * * * * root /usr/bin/clearcache.sh" > /etc/cron.d/clearcache1
 #echo "@reboot root /usr/bin/autokill" > /etc/cron.d/autokill
 #sed -i '$ i\screen -AmdS check /root/autokill' /etc/rc.local
 
+# pendukung shc
 cd
+apt-get install yum
+yum -y install make automake autoconf gcc gcc++
+apt-get -y install build-essential
+aptitude -y install build-essential
+cd
+
+#shc
+wget $source/debian7/shc-3.8.7.tgz"
+tar xvfz shc-3.8.7.tgz
+clear
+echo "=========================================================="
+echo "-------------------Tanggal Kadaluarsa MENU----------------"
+echo "##########################################################"
+echo -e "Wajib di isi bos yusuf ardiansyah" | boxes -d peek | lolcat
+
+echo -e "Contoh Format Tanggal: 30/07/2018 (2 digit/2 digit/4 digit)"
+echo -e "Angka semua ya boss!" | lolcat
+echo ""
+read -p "Silahkan Ketikan Tanggal Kadaluarsa (menu): " deeniemenu
+cd shc-3.8.7
+make
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/menu
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/benchmark
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/speedtest
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/ps-mem
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/dropmon
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-active-list
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-add
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-add-pptp
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-del
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/disable-user-expire
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/delete-user-expire
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/banned-user
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-expire-list
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-gen
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-limit
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-list
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-login
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/user-pass
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/clearcache.sh
+./shc -e $deeniemenu -m "Maaf boss MENU ente sudah kadaluarsa silahkan hubungi admin Deny siswanto atau Yusuf ardiansyah" -f /usr/bin/menu-update-script-vps.sh
+
+
+clear
+#./shc -e $deeniemenu -f /usr/local/bin/menu
+#./shc -f /usr/local/bin/menu
+cd
+mv /usr/local/bin/menu.x /usr/local/bin/menu
+chmod +x /usr/local/bin/menu
+mv /usr/local/bin/menu-update-script-vps.sh.x /usr/local/bin/menu-update-script-vps.sh
+cd
+
+
+rm /usr/bin/menu.x.c
+rm /usr/bin/menu-update-script-vps.sh.x.c
+# hapus installan shc
+rm -rf /root/shc-3.8.7
+rm /root/shc-3.8.7.tgz
+
 # swap ram
 dd if=/dev/zero of=/swapfile bs=1024 count=1024k
 # buat swap
