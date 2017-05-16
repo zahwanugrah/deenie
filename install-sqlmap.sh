9129708eb9d368d94ccff1ebd1ff7590a00b46cf
elangoverdosis <deeniedoank@gmail.com>
2017-05-16 05:21:35 +0000

    Update install-sqlmap.sh

--- install-sqlmap.sh
+++ install-sqlmap.sh
@@ -1,6 +1,48 @@
+#!/bin/bash
 
+if [[ $USER != "root" ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
+if [[ $ether = "" ]]; then
+        ether=eth0
+fi
+
+#vps="zvur";
+vps="aneka";
+
+#if [[ $vps = "zvur" ]]; then
+	#source="http://"
+#else
+	source="https://raw.githubusercontent.com/elangoverdosis/deeniedoank"
+#fi
+
+# go to root
+cd
+
+# check registered ip
+wget -q -O IP $source/debian7/IP.txt
+if ! grep -w -q $MYIP IP; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "zvur" ]]; then
+		echo "Hubungi: editor ( elang overdoasis atau deeniedoank)"
+	else
+		echo "Hubungi: editor ( elang overdoasis atau deeniedoank)"
+	fi
+	rm -f /root/IP
+	exit
+fi
 cd
 apt-get install git
 git clone https://github.com/elangoverdosis/sqlmap.git
 cd sqlmap
 chmod 755 sqlmap.py
+cd /root
+
