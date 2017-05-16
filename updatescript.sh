8d209bda75869f4ce765fe960299e3969c44178d
elangoverdosis <deeniedoank@gmail.com>
2017-05-16 05:25:26 +0000

    Update updatescript.sh

--- updatescript.sh
+++ updatescript.sh
@@ -1,4 +1,46 @@
 #!/bin/bash
+#!/bin/bash
+
+if [[ $USER != "root" ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# initialisasi var
+export DEBIAN_FRONTEND=noninteractive
+OS=`uname -m`;
+MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP2="s/xxxxxxxxx/$MYIP/g";
+ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
+if [[ $ether = "" ]]; then
+        ether=eth0
+fi
+
+#vps="zvur";
+vps="aneka";
+
+#if [[ $vps = "zvur" ]]; then
+	#source="http://"
+#else
+	source="https://raw.githubusercontent.com/elangoverdosis/deeniedoank"
+#fi
+
+# go to root
+cd
+
+# check registered ip
+wget -q -O IP $source/debian7/IP.txt
+if ! grep -w -q $MYIP IP; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "zvur" ]]; then
+		echo "Hubungi: editor ( elang overdoasis atau deeniedoank)"
+	else
+		echo "Hubungi: editor ( elang overdoasis atau deeniedoank)"
+	fi
+	rm -f /root/IP
+	exit
+fi
+clear
 #echo "Maaf.. Saat ini belum tersedia update script bos......!!!" | boxes -d boy
 #echo "Update webmin tersedia bos, tp versi webmin 1.820_all.deb..gk apa2 ya boss.." | lolcat
 echo "Perbaikan MENU unbanned user" | boxes -d boy | lolcat
