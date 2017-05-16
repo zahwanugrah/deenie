1f90e7b4d01d0d45e0a37baaef1902e79ec2161a
elangoverdosis <deeniedoank@gmail.com>
2017-05-16 14:21:02 +0000

    Update menu-update-script-vps.sh

--- menu-update-script-vps.sh
+++ menu-update-script-vps.sh
@@ -10,42 +10,7 @@ function update_script() {
 cd
 wget -O /usr/bin/updatescript.sh $source/debian7/updatescript.sh && clear && chmod +x /usr/bin/updatescript.sh && /usr/bin/updatescript.sh && rm -f /root/IP && rm -f /root/IPcarding
 }
-function carding_hack() {
-clear
-echo "Apa Anda Sudah Install Sqlmap, Klo Belum Install Dulu Boss!!!" | lolcat
-	read -p "Klik ENTER untuk lanjut..."
-	if [[ ! -e /root/sqlmap ]]; then
-	echo "Maaf boss sqlmap belum di install, Silahkan install dulu :p" | lolcat
-	exit
-	fi
-	cd
-	PS3='Silahkan pilih (1-4):'
-options=("Cari Web Vuln" "Hasil Pencarian Web Vuln" "Hacked" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-	"Cari Web Vuln")
-	 web-vuln.sh
-	 break
-	     ;;
-	  "Hasil Pencarian Web Vuln")
-	  clear
-	  less /root/uniscan6.2/sites.txt
-	  break
-	  ;;
-	     "Hacked")
-	     carding.sh
-	     break
-	    ;;
-	    "Quit")
-	    exit
-	    break
-	    ;;
-	    
-	    *) echo invalid option;
-	    esac
-	    done
-	    }
+
 clear
 echo "SELAMAT DATANG DI MENU UPDATE SCRIPT VPS" | boxes -d dog | lolcat
 PS3='Silahkan ketik angka 1 untuk update script lalu ENTER: '
