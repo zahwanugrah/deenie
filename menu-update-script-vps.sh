ac2a3d99e2cc50ed60a2028ba1d4116f8a61ba9a
elangoverdosis <deeniedoank@gmail.com>
2017-05-16 10:24:02 +0000

    Update menu-update-script-vps.sh

--- menu-update-script-vps.sh
+++ menu-update-script-vps.sh
@@ -19,14 +19,14 @@ echo "Apa Anda Sudah Install Sqlmap, Klo Belum Install Dulu Boss!!!" | lolcat
 	exit
 	fi
 	cd
-	PS3='Silahkan Pilih (1-3): '
-	options=("Web VULN" "Hacked" "Quit")
-	select opt in " ${options[@]}"
-	do
-	 case $opt in
-	 "Web VULN")
-	     web-vuln.sh
-	     break
+	PS3='Silahkan pilih (1-2):'
+options=("Web VULN" "Hacked" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+	"Web VULN")
+	 web-vuln.sh
+	 break
 	     ;;
 	     "Hacked")
 	     carding.sh
