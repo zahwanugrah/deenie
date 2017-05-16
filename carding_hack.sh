2902ce2561f011d7e07f94e15caa9e2f0e31996f
elangoverdosis <deeniedoank@gmail.com>
2017-05-16 14:22:33 +0000

    Create carding_hack.sh

---
+++ carding_hack.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+clear
+echo "Apa Anda Sudah Install Sqlmap, Klo Belum Install Dulu Boss!!!" | lolcat
+	read -p "Klik ENTER untuk lanjut..."
+	if [[ ! -e /root/sqlmap ]]; then
+	echo "Maaf boss sqlmap belum di install, Silahkan install dulu :p" | lolcat
+	exit
+	fi
+	cd
+	PS3='Silahkan pilih (1-4):'
+options=("Cari Web Vuln" "Hasil Pencarian Web Vuln" "Hacked" "Quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+	"Cari Web Vuln")
+	 web-vuln.sh
+	 break
+	     ;;
+	  "Hasil Pencarian Web Vuln")
+	  clear
+	  less /root/uniscan6.2/sites.txt
+	  break
+	  ;;
+	     "Hacked")
+	     carding.sh
+	     break
+	    ;;
+	    "Quit")
+	    exit
+	    break
+	    ;;
+	
+	    *) echo invalid option;
+	    esac
+	    done
