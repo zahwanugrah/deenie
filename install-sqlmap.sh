7c8e3e6926ffdae305fb9ec3d8c1e964db05b62a
elangoverdosis <deeniedoank@gmail.com>
2017-05-17 17:26:46 +0000

    Update install-sqlmap.sh

--- install-sqlmap.sh
+++ install-sqlmap.sh
@@ -8,7 +8,9 @@ fi
 # initialisasi var
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
-MYIP=$(wget -qO- ipv4.icanhazip.com);
+#MYIP=$(wget -qO- ipv4.icanhazip.com);
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+#myint=`ifconfig | grep -B1 "inet addr:$myip" | head -n1 | awk '{print $1}'`;
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 ether=`ifconfig | cut -c 1-8 | sort | uniq -u | grep venet0 | grep -v venet0:`
 if [[ $ether = "" ]]; then
