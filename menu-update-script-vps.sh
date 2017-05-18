c0db5605be0bb437779107a4f6821f544c9773db
elangoverdosis <deeniedoank@gmail.com>
2017-05-18 10:58:07 +0000

    Update menu-update-script-vps.sh

--- menu-update-script-vps.sh
+++ menu-update-script-vps.sh
@@ -78,7 +78,7 @@ q
 clear
 echo "SELAMAT DATANG DI MENU UPDATE SCRIPT VPS" | boxes -d dog | lolcat
 PS3='Silahkan ketik angka 1 untuk update script lalu ENTER: '
-options=("Update Script VPS" "Install DDOS deflate"Install SQLmap" "Carding" "Quit")
+options=("Update Script VPS" "Install DDOS deflate" "Install SQLmap" "Carding" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
