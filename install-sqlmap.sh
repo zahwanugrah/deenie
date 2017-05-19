e679ee00d4131f8a233fe9dea6aaaa280f7fb92e
elangoverdosis <deeniedoank@gmail.com>
2017-05-19 16:40:30 +0000

    Update install-sqlmap.sh

--- install-sqlmap.sh
+++ install-sqlmap.sh
@@ -93,8 +93,8 @@ apt-get install curl
 git clone https://github.com/elangoverdosis/bingoo.git
 clear
 # phpkit
-wget https://raw.githubusercontent.com/elangoverdosis/PHPKIT/phpkit.sh
-wget https://raw.githubusercontent.com/elangoverdosis/PHPKIT/pHpKit-SQLi.rb
+wget https://raw.githubusercontent.com/elangoverdosis/PHPKIT/master/pHpKit-SQLi.rb
+wget https://raw.githubusercontent.com/elangoverdosis/PHPKIT/master/phpkit.sh
 clear
 chmod +x ./pHpKit-SQLi.rb
 chmod +x ./phpkit.sh
