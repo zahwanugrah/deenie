930d7cfc47b45c2f76ef8f9c952f4756e42a28c4
elangoverdosis <deeniedoank@gmail.com>
2017-05-19 04:38:21 +0000

    Update install-sqlmap.sh

--- install-sqlmap.sh
+++ install-sqlmap.sh
@@ -70,10 +70,10 @@ chmod +x /usr/bin/web-vuln.sh
 clear
 cd /root
 
-wget -O /usr/bin/carding_hack $source/debian7/carding_hack.sh
-chmod +x /usr/bin/carding_hack
-clear
-cd
+#wget -O /usr/bin/carding_hack $source/debian7/carding_hack.sh
+#chmod +x /usr/bin/carding_hack
+#clear
+#cd
 
 wget $source/debian7/uniscan6.2.tar.gz
 tar xf uniscan6.2.tar.gz
