0cacb8d21abbeaebf8b7b6f9294cdbd3d0db986e
elangoverdosis <deeniedoank@gmail.com>
2017-05-19 02:40:17 +0000

    Update menu-update-script-vps.sh

--- menu-update-script-vps.sh
+++ menu-update-script-vps.sh
@@ -55,25 +55,25 @@ mkdir /usr/local/ddos
 fi
 clear
 echo; echo "Installing DOS-Deflate 0.6"; echo
-echo; echo -n "Downloading source files..."
+echo; echo -n 'Downloading source files...'
 wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
-echo -n "."
+echo -n '.'
 wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
-echo -n "."
+echo -n '.'
 wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
-echo -n "."
+echo -n '.'
 wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
 chmod 0755 /usr/local/ddos/ddos.sh
 cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
-echo "...done"
+echo '...done'
 echo; echo -n 'Creating cron to run script every minute.....(Default setting)'
 /usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
-echo ".....done"
-echo; echo "Installation has completed."
-echo "Config file is at /usr/local/ddos/ddos.conf"
-echo "Please send in your comments and/or suggestions to zaf@vsnl.com"
-sleep 3
-q
+echo '.....done'
+echo; echo 'Installation has completed.'
+echo 'Config file is at /usr/local/ddos/ddos.conf'
+echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
+
+
 }
 clear
 echo "SELAMAT DATANG DI MENU UPDATE SCRIPT VPS" | boxes -d dog | lolcat
