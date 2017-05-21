6e2d4ad2a02262b2cc481caa592c65b299fe6072
elangoverdosis <deeniedoank@gmail.com>
2017-05-21 12:07:32 +0000

    Update installovpn.sh

--- installovpn.sh
+++ installovpn.sh
@@ -1,13 +1,51 @@
 #!/bash
 
+MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+source="https://raw.githubusercontent.com/elangoverdosis/deeniedoank"
+#fi
+
+# go to root
+cd
+
+# check registered ip
+wget -q -O IP $source/debian7/IP.txt
+if ! grep -w -q $MYIP IP; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!"
+	if [[ $vps = "zvur" ]]; then
+		echo "Hubungi: editor ( elang overdoasis atau deeniedoank)"
+	else
+		echo "Hubungi: editor ( elang overdoasis atau deeniedoank)"
+	fi
+	rm /root/IP
+	rm -f /root/IP
+	exit
+fi
 # install openvpn
+#myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
 apt-get install openvpn -y
-wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/denysiswanto/autoscript2/master/conf/openvpn-debian.tar"
+wget -O /etc/openvpn/openvpn.tar $source/debian7/openvpn-debian.tar
 cd /etc/openvpn/
 tar xf openvpn.tar
-wget -O /etc/openvpn/1194.conf "https://raw.github.com/denysiswanto/autoscript2/master/conf/1194.conf"
+wget -O /etc/openvpn/1194.conf $source/debian7/1194.conf
 service openvpn restart
 sysctl -w net.ipv4.ip_forward=1
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-wget -O /etc/iptables.conf "https://raw.github.com/denysiswanto/autoscript2/master/conf/iptables.conf"
+wget -O /etc/iptables.conf $source/debian7/iptables.conf
 sed -i '$ i\iptables-restore < /etc/iptables.conf' /etc/rc.local
+
+
+sed -i 's/ipserver/$MYIP/g' /etc/iptables.conf
+
+iptables-restore < /etc/iptables.conf
+service openvpn restart
+
+# configure openvpn client config
+cd /etc/openvpn/
+wget -O /etc/openvpn/client.ovpn $source/debian7/1194-client.conf
+
+cp /etc/openvpn/client.ovpn /home/vps/public_html/client.ovpn
+sed -i 's/ipserver/$MYIP/g' /home/vps/public_html/client.ovpn
+sed -i 's/ports/55/' /home/vps/public_html/client.ovpn
