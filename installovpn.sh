833480699fa473a75b5e61ed9a139e1ae0a6c91d
elangoverdosis <deeniedoank@gmail.com>
2017-05-21 12:17:26 +0000

    Update installovpn.sh

--- installovpn.sh
+++ installovpn.sh
@@ -9,7 +9,7 @@ fi
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
 #MYIP=$(wget -qO- ipv4.icanhazip.com);
-fi
+
 # get the VPS IP
 #ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
 MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
