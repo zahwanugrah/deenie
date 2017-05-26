808f4add8c0bb5797e2a4bf0faa8bc202151e853
elangoverdosis <deeniedoank@gmail.com>
2017-05-26 06:04:15 +0000

    Update install-sqlmap.sh

--- install-sqlmap.sh
+++ install-sqlmap.sh
@@ -140,8 +140,7 @@ clear
 cd
 git clone https://github.com/elangoverdosis/ptf.git
 cd ptf
-./ptf
-use modules/exploitation/install_update_all,modules/post-exploitation/install_update_all,modules/vulnerability-analysis/install_update_all
+./ptf && use modules/exploitation/install_update_all,modules/post-exploitation/install_update_all,modules/vulnerability-analysis/install_update_all
 use
 
 cd
