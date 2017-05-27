e08c1bb2995b8d32448c854ed67cedbf82f63e86
elangoverdosis <deeniedoank@gmail.com>
2017-05-27 10:06:26 +0000

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -362,12 +362,15 @@ wget -O /usr/bin/clearcache.sh $source/debian7/clearcache.sh
 wget -O /usr/bin/bannermenu $source/debian7/bannermenu
 wget -O /usr/bin/menu-update-script-vps.sh $source/debian7/menu-update-script-vps.sh
 cd
-
-echo "0 */6 * * * root service dropbear restart" > /etc/cron.d/dropbear
+#rm -rf /etc/cron.weekly/
+#rm -rf /etc/cron.hourly/
+#rm -rf /etc/cron.monthly/
+rm -rf /etc/cron.daily/
+echo "0 */2 * * * root service dropbear restart" > /etc/cron.d/dropbear
 echo "00 23 * * * root /usr/bin/disable-user-expire" > /etc/cron.d/disable-user-expire
 echo "0 */12 * * * root /sbin/reboot" > /etc/cron.d/reboot
 #echo "00 01 * * * root echo 3 > /proc/sys/vm/drop_caches && swapoff -a && swapon -a" > /etc/cron.d/clearcacheram3swap
-echo "0 */4 * * * root /usr/bin/clearcache.sh" > /etc/cron.d/clearcache1
+echo "0 */1 * * * root /usr/bin/clearcache.sh" > /etc/cron.d/clearcache1
 
 cd
 chmod +x /usr/bin/benchmark
