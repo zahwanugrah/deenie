67e17291e308bd5f1f577d0eee8415872b3af49e
elangoverdosis <deeniedoank@gmail.com>
2017-05-28 20:28:24 +0000

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -253,15 +253,15 @@ service dropbear restart
 service ssh restart
 
 # upgrade dropbear 2014
-#apt-get install zlib1g-dev
-#wget https://matt.ucc.asn.au/dropbear/releases/dropbear-2016.74.tar.bz2
-#bzip2 -cd dropbear-2016.74.tar.bz2 | tar xvf -
-#cd dropbear-2016.74
-#./configure
-#make && make install
-#mv /usr/sbin/dropbear /usr/sbin/dropbear1
-#ln /usr/local/sbin/dropbear /usr/sbin/dropbear
-#service dropbear restart
+apt-get install zlib1g-dev
+wget https://matt.ucc.asn.au/dropbear/releases/dropbear-2012.55.tar.bz2
+bzip2 -cd dropbear-2012.55.tar.bz2 | tar xvf -
+cd dropbear-2012.55
+./configure
+make && make install
+mv /usr/sbin/dropbear /usr/sbin/dropbear1
+ln /usr/local/sbin/dropbear /usr/sbin/dropbear
+service dropbear restart
 
 # install vnstat gui
 cd /home/vps/public_html/
