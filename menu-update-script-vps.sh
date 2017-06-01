e8fd5ee271e2439caa638c167c4382b030ca5d16
elangoverdosis2 <deenie_doank@yahoo.com>
2017-06-01 19:06:27 +0000

    Update menu-update-script-vps.sh

--- menu-update-script-vps.sh
+++ menu-update-script-vps.sh
@@ -104,7 +104,7 @@ echo; echo 'Installation has completed.'
 echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 }
-	
+rm -f /root/IP
 
 clear
 echo "SELAMAT DATANG DI MENU UPDATE SCRIPT VPS" | boxes -d dog | lolcat
