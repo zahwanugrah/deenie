09afac2668780e13dbdc3145835688e8cf13639c
elangoverdosis2 <deenie_doank@yahoo.com>
2017-06-01 11:16:12 +0700

    Update carding_hack.sh

--- carding_hack.sh
+++ carding_hack.sh
@@ -26,7 +26,7 @@ fi
 #if [[ $vps = "zvur" ]]; then
 	#source="http://"
 #else
-	source="https://raw.githubusercontent.com/elangoverdosis/deeniedoank"
+	source="https://raw.githubusercontent.com/elangoverdosis2/cinta"
 #fi
 
 # go to root
