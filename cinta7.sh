215989bab93baa38f3ecbab95cfafe7b8411c73c
elangoverdosis2 <deenie_doank@yahoo.com>
2017-06-01 15:48:41 +0000

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -187,13 +187,13 @@ service nginx restart
 cd
 
 # install badvpn
-wget -O /usr/bin/badvpn-udpgw $source/debian7/badvpn-udpgw
-if [[ $OS == "x86_64" ]]; then
-  wget -O /usr/bin/badvpn-udpgw $source/debian7/badvpn-udpgw64
-fi
-sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
-chmod +x /usr/bin/badvpn-udpgw
-screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
+#wget -O /usr/bin/badvpn-udpgw $source/debian7/badvpn-udpgw
+#if [[ $OS == "x86_64" ]]; then
+#wget -O /usr/bin/badvpn-udpgw $source/debian7/badvpn-udpgw64
+#fi
+#sed -i '$ i\screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300' /etc/rc.local
+#chmod +x /usr/bin/badvpn-udpgw
+#screen -AmdS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7300
 cd
 
 # install mrtg
