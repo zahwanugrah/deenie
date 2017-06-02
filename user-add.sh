bc4a2fc41ab2f0669a51a7294a57982f4a8251d4
elangoverdosis2 <deenie_doank@yahoo.com>
2017-06-02 07:29:05 +0000

    Update user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -49,7 +49,7 @@ echo -e "     Port default dropbear: 443,80         " | lolcat
 echo -e "     Port default openSSH : 22,143         " | lolcat
 echo -e "     Port default squid   : 8080,3128      " | lolcat
 echo -e "                                           " | lolcat
-echo -e "     Auto kill user maximal login 2        " | lolcat
+echo -e "     Peringatan: Maksimal 2 login          " | lolcat
 echo -e "-------------------------------------------" | lolcat
 echo -e "     Aktif Sampai: $(date -d "$AKTIF days" +"%d-%m-%Y")" | lolcat
 echo -e "===========================================" | lolcat
