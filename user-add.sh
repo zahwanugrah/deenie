155c1efbb691927e813e231a16a8eda34afc80b8
elangoverdosis2 <deenie_doank@yahoo.com>
2017-06-10 07:17:27 +0000

    Create user-add.sh

--- user-add.sh
+++ user-add.sh
@@ -15,10 +15,8 @@ fi
 #MYIP=$(wget -qO- ipv4.icanhazip.com)
 
 
-echo "------------------------ MEMBUAT AKUN SSH ------------------------"| lolcat
-
-
-	echo "           DEVELOPED BY YUSUF ARDIANSYAH AND ELANG OVERDOSIS           " | lolcat
+echo "------------------------ MEMBUAT AKUN SSH ------------------------"
+echo "           DEVELOPED BY YUSUF ARDIANSYAH AND ELANG OVERDOSIS           " 
 echo ""
 
 read -p "Isikan username: " username
@@ -39,28 +37,28 @@ clear
 echo -e ""
 echo -e ""
 echo -e ""
-echo -e ""| lolcat
-echo -e "|       Informasi Akun Baru SSH      |" | boxes -d dog | lolcat
-echo -e "============[[-SERVER-PREMIUM-]]===========" | lolcat
-echo -e "     Host: $MYIP" | lolcat
-echo -e "     Username: $username" | lolcat
-echo -e "     Password: $password                   " | lolcat
-echo -e "     Port default dropbear: 443,80         " | lolcat
-echo -e "     Port default openSSH : 22,143         " | lolcat
-echo -e "     Port default squid   : 8080,3128      " | lolcat
-echo -e "                                           " | lolcat
-echo -e "     Peringatan: Maksimal 2 login          " | lolcat
-echo -e "-------------------------------------------" | lolcat
-echo -e "     Aktif Sampai: $(date -d "$AKTIF days" +"%d-%m-%Y")" | lolcat
-echo -e "===========================================" | lolcat
-echo -e "   DI LARANG:                              "| lolcat
-echo -e "   HACKING-DDOS-PHISING-SPAM-TORENT        " | lolcat
-echo -e "   CARDING-CRIMINAL CYBER.                 "| lolcat
-echo -e "===========================================" | lolcat
-echo -e "   Script by Yusuf Ardiansyah              "| lolcat
-echo -e "   Config OVPN:                            "| lolcat
-echo -e "   http://$MYIP:81/client.ovpn        " | lolcat
-echo -e "-------------------------------------------" | lolcat
+echo -e "==========================================="
+echo -e "|         Informasi Akun Baru SSH         |" 
+echo -e "============[[-SERVER-PREMIUM-]]===========" 
+echo -e "     Host: $MYIP" 
+echo -e "     Username: $username" 
+echo -e "     Password: $password                   "
+echo -e "     Port default dropbear: 443,80         " 
+echo -e "     Port default openSSH : 22,143         " 
+echo -e "     Port default squid   : 8080,3128      " 
+echo -e "                                           " 
+echo -e "     Peringatan: Maksimal 2 login          " 
+echo -e "-------------------------------------------"
+echo -e "     Aktif Sampai: $(date -d "$AKTIF days" +"%d-%m-%Y")" 
+echo -e "===========================================" 
+echo -e "   DI LARANG:                              "
+echo -e "   HACKING-DDOS-PHISING-SPAM-TORENT        " 
+echo -e "   CARDING-CRIMINAL CYBER.                 "
+echo -e "===========================================" 
+echo -e "   Script by Yusuf Ardiansyah              "
+echo -e "   Config OVPN:                            "
+echo -e "   http://$MYIP:81/client.ovpn        " 
+echo -e "-------------------------------------------"
 echo -e ""
 echo -e ""
 fi
