0f14f0a774fd560a09c705d4b79d5fd6435afc01
elangoverdosis2 <deenie_doank@yahoo.com>
2017-06-19 05:22:06 +0000

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -251,7 +251,8 @@ service squid3 restart
 
 # install webmin
 cd
-wget -O webmin-current.deb $source/debian7/webmin-current.deb
+wget -O webmin-current.deb http://prdownloads.sourceforge.net/webadmin/webmin_1.760_all.deb
+#wget -O webmin-current.deb $source/debian7/webmin-current.deb
 dpkg -i --force-all webmin-current.deb
 apt-get -y -f install;
 #sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
