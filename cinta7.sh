fc2c6f4b21c43f9f6d2530cf45f393b310cf5085
elangoverdosis88 <34495547+elangoverdosis88@users.noreply.github.com>
2017-12-20 11:02:13 +0700

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -361,7 +361,7 @@ apt-get update
 apt-get upgrade
 apt-get install stunnel4 -y
 wget -O /etc/stunnel/stunnel.conf $source/debian7/stunnel.conf
-sed -i 's/ipserver/$MYIP/g' /etc/stunnel/stunnel.conf
+#sed -i 's/ipserver/$MYIP/g' /etc/stunnel/stunnel.conf
 openssl genrsa -out key.pem 2048
 openssl req -new -x509 -key key.pem -out cert.pem -days 1095
 cat key.pem cert.pem >> /etc/stunnel/stunnel.pem
