70070044e5ba6b0fee820761b3bf728dabbf458a
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2018-07-14 10:47:07 +0800

    Update pptp.sh

--- pptp.sh
+++ pptp.sh
@@ -11,7 +11,7 @@ MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*
 vps="aneka";
 
 
-	source="https://raw.githubusercontent.com/elangoverdosis88/deenie"
+	source="https://raw.githubusercontent.com/zahwanugrah/deenie"
 
 
 # go to root
