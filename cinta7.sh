0307293cc3a8780d27ad4b4750e1ba762fc3144b
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2018-07-30 22:57:46 +0800

    Update cinta7.sh

--- cinta7.sh
+++ cinta7.sh
@@ -266,9 +266,10 @@ service squid3 restart
 
 # install webmin
 cd
-#wget -O webmin-current.deb http://prdownloads.sourceforge.net/webadmin/webmin_1.870_all.deb
-wget -O webmin-current.deb $source/debian7/webmin-current.deb
-dpkg -i --force-all webmin-current.deb
+wget -O webmin-current.deb http://prdownloads.sourceforge.net/webadmin/webmin_1.890_all.deb
+dpkg -i webmin_1.890_all.deb
+#wget -O webmin-current.deb $source/debian7/webmin-current.deb
+#dpkg -i --force-all webmin-current.deb
 sudo apt-get -y -f install;
 sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
 rm -f /root/webmin-current.deb
